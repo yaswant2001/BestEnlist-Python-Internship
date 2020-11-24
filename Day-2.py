9350caa99a8be93b8913d28ec44e0ebaf29db583
KURAGONDA YASWANTH <73974566+yaswant2001@users.noreply.github.com>
2020-11-24 12:59:40 +0530

    Day-2
    #30dayspythonchallenge #BestEnlist

---
+++ Day-2.py
@@ -0,0 +1,18 @@
+#first challenge
+a=10;
+b=50;
+c=100;
+div=a/10;
+multi=b*50;
+add=c+60;
+print("div:",div);
+print("multi:",multi);
+print("add:",add);
+#seond challenge
+str="aeiou";
+print(str.replace('o','G'));
+#third challenge
+a=150;
+b=33.4;
+print(float(a));
+print(int(b));
