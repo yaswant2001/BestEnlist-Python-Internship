26d9b1031024c7c90b06454e9f0cff0446d70c6f
KURAGONDA YASWANTH <yaswanthkurugonda200@gmail.com>
2020-11-29 23:45:10 +0530

    Day-7

---
+++ Day-7.py
@@ -0,0 +1,20 @@
+#1
+import main
+
+print("initial age:",main.age)
+
+updated_age=[x+2 for x in main.age]
+
+print("Updated age:",updated_age)
+#2
+import pandas as pd
+age=[15,24,45,50,75,100,8]
+age2=pd.Series(age)
+print(age2)
+#3
+import sys
+sys.path
+#4
+pip install pandas #install package
+
+pip uninstall pandas#uninstall package
